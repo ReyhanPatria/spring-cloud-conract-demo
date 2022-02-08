9079734f2a8ecbdb17a301439fcb05409cddb827
ReyhanPatria <reyhan2patria@gmail.com>
2022-02-08 22:23:44 +0700

    Added implementations onto test classes

--- consumer/src/test/java/com/example/consumer/LoanTests.java
+++ consumer/src/test/java/com/example/consumer/LoanTests.java
@@ -1,11 +1,19 @@
 package com.example.consumer;
 
+import com.example.consumer.service.LoanService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 public class LoanTests {
+    @Autowired
+    public LoanService loanService;
+
     @Test
     public void shouldSucceedFraudCheck() {
         // given:
@@ -29,5 +37,4 @@ public class LoanTests {
         // then:
         assertFalse(result);
     }
-}
 }
