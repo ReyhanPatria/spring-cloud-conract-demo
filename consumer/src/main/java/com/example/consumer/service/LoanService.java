a6dccd61fa01b327e1425fef2089604ff2c311ed
ReyhanPatria <reyhan2patria@gmail.com>
2022-02-09 22:48:56 +0700

    Changed request body handling

--- consumer/src/main/java/com/example/consumer/service/LoanService.java
+++ consumer/src/main/java/com/example/consumer/service/LoanService.java
@@ -4,12 +4,17 @@ import com.example.consumer.repository.FraudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class LoanService {
     @Autowired
     private FraudRepository fraudRepository;
 
-    public Boolean apply(Integer amount) {
-        return fraudRepository.checkApplication(amount);
+    public Map<String, String> apply(Integer amount) {
+        Map<String, Integer> body = new HashMap<>();
+        body.put("amount", amount);
+        return fraudRepository.checkApplication(body);
     }
 }
